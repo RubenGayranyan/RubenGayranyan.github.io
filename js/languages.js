bbb9efdcc9a1e7826278020a9851e238fc3fa785
RubenGayranyan <ruben.gayranyan@gmail.com>
2025-09-16 18:50:58 +0400

    Added script.js, modified CSS.

--- js/languages.js
+++ js/languages.js
@@ -6,7 +6,8 @@ const translations = {
         about: "👩‍💻 Տեխնոլոգիաների և ծրագրավորման սիրահար եմ․ զբաղվում եմ վեբ, AI, դեսկթոփ և Android նախագծերով։",
         about2: "🎓 Սովորում եմ Հայաստանի Ֆրանսիական համալսարանում՝ Ինֆորմատիկա և կիրառական մաթեմատիկա ֆակուլտետում։",
         about_last: "🚀 Միշտ բաց եմ նոր մարտահրավերների և նախագծերի, որոնք զարգացնում են իմ հմտությունները։ ✨",
-        tg_join: "Միացի՛ր իմ ալիքին"
+        tg_join: "Միացի՛ր իմ ալիքին",
+        visits: "Դիտումներ՝"
     },
     en: {
         name_surname: "Ruben Gayrayan",
@@ -15,7 +16,8 @@ const translations = {
         about: "👩‍💻 I am passionate about technology and programming, working on Web, AI, Desktop, and Android projects.",
         about2: "🎓 I study at the French University in Armenia, Faculty of Informatics and Applied Mathematics.",
         about_last: "🚀 Always open to new challenges and projects that help me grow my skills. ✨",
-        tg_join: "Join my channel"
+        tg_join: "Join my channel",
+        visits: "Visits:"
     },
     ru: {
         name_surname: "Рубен Гайранян",
@@ -24,7 +26,8 @@ const translations = {
         about: "👩‍💻 Увлекаюсь технологиями и программированием, занимаюсь веб, AI, десктоп и Android проектами.",
         about2: "🎓 Учусь во Французском университете в Армении, факультет информатики и прикладной математики.",
         about_last: "🚀 Всегда открыт новым вызовам и проектам, которые развивают мои навыки. ✨",
-        tg_join: "Подпишись к моему каналу"
+        tg_join: "Подпишись к моему каналу",
+        visits: "Просмотры:"
     },
     fr: {
         name_surname: "Ruben Gayrayan",
@@ -33,7 +36,8 @@ const translations = {
         about: "👩‍💻 Passionné par la technologie et la programmation, je travaille sur des projets Web, AI, Desktop et Android.",
         about2: "🎓 J’étudie à l’Université Française en Arménie, en Informatique et Mathématiques Appliquées.",
         about_last: "🚀 Toujours ouvert aux nouveaux défis et projets qui développent mes compétences. ✨",
-        tg_join: "Rejoignez ma chaîne"
+        tg_join: "Rejoignez ma chaîne",
+        visits: "Vues:"
     }
 };
 
